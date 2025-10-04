ce91e0dba1bddc5498369a4b1fec4722e2f9be5b
SUCHITHRA <157681264+Suchithrakulal@users.noreply.github.com>
2025-10-04 11:54:23 +0530

    Create cie3c2.c

---
+++ cie3c2.c
@@ -0,0 +1,26 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    int rank;
+    char msg[50];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        strcpy(msg, "Hello from Process 0");
+        MPI_Send(msg, strlen(msg) + 1, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+        MPI_Recv(msg, 50, MPI_CHAR, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process 0 received message: %s\n", msg);
+    } else if (rank == 1) {
+        MPI_Recv(msg, 50, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process 1 received message: %s\n", msg);
+        strcpy(msg, "Hello from Process 1");
+        MPI_Send(msg, strlen(msg) + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
